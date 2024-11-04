9f9d5ba33632c45edbe3d75faf652ae045ffa843
kapwebdr <damien@kapweb.com>
2024-11-04 18:08:31 +0100

    Remove Old Api

--- Install-api.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-python3.12 -m venv ./venv
-source ./venv/bin/activate
-pip install --no-cache-dir llama-cpp-python \
-  --extra-index-url https://abetlen.github.io/llama-cpp-python/whl/metal
-pip install --no-cache-dir  -r requirements.txt
